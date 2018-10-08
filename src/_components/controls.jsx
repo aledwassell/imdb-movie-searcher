3322ce2c80f7811205849dd0c2f3a2aa9401efae
aledwassell <aledwassell@googlemail.com>
2018-10-09 00:32:58 +0100

    doing some styling

---
+++ src/_components/controls.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Controls extends Component {
+    state = {}
+    render() {
+        console.log('props ', this.props);
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Controls;
