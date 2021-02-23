b1c1fab9ffedb1f8123be3dce677b7bb34989845
aledwassell <aledwassell@googlemail.com>
2021-02-23 10:32:54 +0000

    altering the API key

--- src/_components/movies.jsx
+++ src/_components/movies.jsx
@@ -6,14 +6,12 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 
-const API_KEY = '318e5b3b';
-
 class Movies extends Component {
     //state holds all the data the app is going tuse, this data can be mutated using setState()
     state = {
         defaultSearch: 'batman',
         movies: [],
-        moviesUrl: `https://www.omdbapi.com/?apikey=${API_KEY}&s=`,
+        moviesUrl: `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=`,
         listView: true,
         movieView: false,
         selectedMovieId: ''
@@ -42,7 +40,7 @@ class Movies extends Component {
 
             }).then(() => {
                 // reset movieUrl
-            this.setState({moviesUrl: `https://www.omdbapi.com/?apikey=${API_KEY}&s=`});
+            this.setState({moviesUrl: `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=`});
         });
     };
     handleMovieSelect = (movieId) => {
